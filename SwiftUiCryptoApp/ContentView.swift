ed9b0646e69c52827ee9d52f6f9a686ba4266093
ipeerless <ipeerless0@gmail.com>
2025-01-25 09:50:31 +0200

    Initial Commit

---
+++ SwiftUiCryptoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiCryptoApp
+//
+//  Created by ipeerless on 25/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiCryptoApp/SwiftUiCryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCryptoAppApp.swift
+//  SwiftUiCryptoApp
+//
+//  Created by ipeerless on 25/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
