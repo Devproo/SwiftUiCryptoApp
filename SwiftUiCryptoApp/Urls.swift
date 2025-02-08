553e7ed75251bf2287ca45448f6b9298dda53bf7
ipeerless <ipeerless0@gmail.com>
2025-02-08 20:04:00 +0200

    new Urls

---
+++ SwiftUiCryptoApp/Urls.swift
@@ -0,0 +1,12 @@
+//
+//  Urls.swift
+//  SwiftUiCryptoApp
+//
+//  Created by ipeerless on 08/02/2025.
+//
+
+import Foundation
+
+struct Urls {
+    static let apiBaseURL = "https://api.coingecko.com/api/v3"
+}
